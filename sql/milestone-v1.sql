93ffef243f6c9b71fd45fcabaeaded5985c81ca7
am-gooru <ashish@gooru.org>
2019-04-04 15:57:59 +0530

    Schema definition for grade_competency_map table

--- sql/milestone-v1.sql
+++ sql/milestone-v1.sql
@@ -2,7 +2,7 @@
 
 -- This queue table is going to be owned by Nucleus and is part of nucleus core DB
 
-create table milestone_queue (
+CREATE TABLE milestone_queue (
     id bigserial NOT NULL,
     course_id uuid NOT NULL,
     fw_code text NOT NULL,
@@ -24,7 +24,7 @@ COMMENT on COLUMN milestone_queue.status IS '0 means queued, 1 means dispatched 
 
 -- This is the milestone table which maintains map of Milestones to Lessons in specified course
 -- This table will be created in nucleus
-create table milestone_lesson_map (
+CREATE TABLE milestone_lesson_map (
   id bigserial NOT NULL PRIMARY KEY,
   course_id uuid NOT NULL,
   unit_id uuid NOT NULL,
@@ -44,9 +44,11 @@ create table milestone_lesson_map (
 
 CREATE INDEX mlp_cf_idx ON milestone_lesson_map USING btree (course_id, fw_code);
 
+COMMENT on TABLE milestone_lesson_map IS 'Mapping of milestones to lesson in a specific course for a specified framework. All the tx_* fields are GUT specific ones and not FW specific things';
+
 
 -- This table will be created in analytics db. The key is not bigserial as key has to match with what is there in nucleus db
-create table milestone (
+CREATE TABLE milestone (
   id bigint NOT NULL PRIMARY KEY,
   course_id uuid NOT NULL,
   unit_id uuid NOT NULL,
@@ -57,3 +59,25 @@ create table milestone (
 
 CREATE INDEX ms_cf_idx ON milestone USING btree (course_id, fw_code);
 
+-- This table will go into DS DB
+-- This table represents a data structure to store grade competency map which is hydrated view of grade_competency_bounds
+-- It includes competencies only in specific grade and not till that grade.
+
+CREATE TABLE grade_competency_map (
+ id bigserial NOT NULL PRIMARY KEY,
+ grade_id integer NOT NULL,
+ tx_subject_code text NOT NULL,
+ fw_code text NOT NULL,
+ tx_domain_code text NOT NULL,
+ tx_domain_seq integer NOT NULL,
+ tx_comp_code text NOT NULL,
+ tx_comp_seq integer NOT NULL
+);
+
+CREATE INDEX gcm_sf_idx ON grade_competency_map USING btree (tx_subject_code, fw_code);
+CREATE INDEX gcm_g_idx ON grade_competency_map USING btree (grade_id);
+
+ALTER TABLE grade_competency_map ADD CONSTRAINT gcm_fk_dcm_sdc FOREIGN KEY (tx_subject_code, tx_domain_code, tx_comp_code) REFERENCES domain_competency_matrix(tx_subject_code, tx_domain_code, tx_comp_code);
+ALTER TABLE grade_competency_map ADD CONSTRAINT gcm_fk_gm_gsfw FOREIGN KEY (grade_id, tx_subject_code, fw_code) REFERENCES grade_master(id, tx_subject_code, fw_code);
+
+COMMENT on TABLE grade_competency_map IS 'This table represents a data structure to store grade competency map which is hydrated view of grade_competency_bounds. It includes competencies in specific grade and not till that grade.'
